d9f440e900ae2b8aeac1f561e17eeb7c70894399
Yariv Nissim <yariv.nis@me.com>
2018-12-17 22:34:31 -0800

    Refactor CarRentalResponse
    Remove unused properties and mark the optional ones according to the API documentation.

--- CarRental/Model/CarRental.swift
+++ CarRental/Model/CarRental.swift
@@ -11,57 +11,75 @@ import Foundation
 // Used quicktype app to generate this JSON with some modifications.
 // https://itunes.apple.com/us/app/paste-json-as-code-quicktype/id1330801220?mt=12
 
-struct CarRentalResponse: Codable, Hashable {
+struct CarRentalResponse: Decodable, Hashable {
     let results: [CarRentalResult]
 }
 
-struct CarRentalResult: Codable, Hashable {
+struct CarRentalResult: Decodable, Hashable {
     let provider: CarRentalProvider
-    let branchId: String
     let location: CarRentalLocation
-    let airport: String
+    let airport: String?
     let address: CarRentalAddress
     let cars: [CarRentalCar]
+
+    enum CodingKeys: String, CodingKey {
+        case provider = "provider"
+        case location = "location"
+        case airport = "airport"
+        case address = "address"
+        case cars = "cars"
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        provider = try container.decode(CarRentalProvider.self, forKey: .provider)
+        location = try container.decode(CarRentalLocation.self, forKey: .location)
+        airport = try container.decodeIfPresent(String.self, forKey: .airport)
+        address = try container.decode(CarRentalAddress.self, forKey: .address)
+        cars = try container.decodeIfPresent([CarRentalCar].self, forKey: .cars) ?? []
+    }
 }
 
-struct CarRentalAddress: Codable, Hashable {
+struct CarRentalAddress: Decodable, Hashable {
     let line1: String
     let city: String
-    let region: String
+    let region: String?
+    let postalCode: String?
     let country: String
 }
 
-struct CarRentalCar: Codable, Hashable {
+struct CarRentalCar: Decodable, Hashable {
     let vehicleInfo: CarRentalVehicleInfo
-    let rates: [CarRentalRate]
-    let estimatedTotal: CarRentalEstimatedTotal
+    let estimatedTotal: CarRentalPrice?
+    let image: CarRentalImage?
 }
 
-struct CarRentalEstimatedTotal: Codable, Hashable {
+struct CarRentalImage: Decodable, Hashable {
+    let width: Double
+    let height: Double
+    let url: URL
+}
+
+struct CarRentalPrice: Decodable, Hashable {
     let amount: String
     let currency: String
 }
 
-struct CarRentalRate: Codable, Hashable {
-    let type: String
-    let price: CarRentalEstimatedTotal
-}
-
-struct CarRentalVehicleInfo: Codable, Hashable {
+struct CarRentalVehicleInfo: Decodable, Hashable {
     let acrissCode: String
-    let transmission: String
-    let fuel: String
-    let airConditioning: Bool
     let category: String
-    let type: String
+    let type: String?
+    let transmission: String?
+    let airConditioning: Bool?
+    let fuel: String?
 }
 
-struct CarRentalLocation: Codable, Hashable {
+struct CarRentalLocation: Decodable, Hashable {
     let latitude: Double
     let longitude: Double
 }
 
-struct CarRentalProvider: Codable, Hashable {
+struct CarRentalProvider: Decodable, Hashable {
     let companyCode: String
     let companyName: String
 }
