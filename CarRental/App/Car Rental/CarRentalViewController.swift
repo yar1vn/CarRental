d992eadf227b3239779e0c8559e2468c86b3bd35
Yariv Nissim <yariv.nis@me.com>
2018-12-21 22:44:52 -0800

    Update CarRentalViewController to initialize its View Model

--- CarRental/App/Car Rental/CarRentalViewController.swift
+++ CarRental/App/Car Rental/CarRentalViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 final class CarRentalViewController: UITableViewController {
-    
+    @IBOutlet private var loadingIndicator: UIActivityIndicatorView!
+
+    private let viewModel = CarRentalViewModel(
+        carRentalService: CarRentalService(apiKey: "ClckdS45oGCo1izQRM7vGR2U2AC3vdhm"),
+        cellIdentifier: "RentalCell"
+    )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.stateChanged = self.handleState
+        tableView.dataSource = viewModel
+    }
+
+    func handleState(state: CarRentalViewModel.State) {
+        DispatchQueue.main.async {
+            self.navigationItem.titleView = nil
+
+            switch state {
+            case .loading:
+                self.navigationItem.titleView = self.loadingIndicator
+            case .rentals, .empty:
+                self.tableView.reloadData()
+            case .error(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
